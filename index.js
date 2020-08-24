0c2b18b7de8cb7712ca907c8195355e4ec1e9941
user <user@MacBook-Air-de-user.local>
2020-08-24 00:42:38 -0300

    Se agregan funciones sicronicas

--- index.js
+++ index.js
@@ -17,59 +17,50 @@ let channelId;
 let cryptoRes = "";
 
 bot.on("text", (ctx) => {
+  setChannelId(ctx);
+});
+
+function setChannelId(ctx) {
   if (!channelId) {
     channelId =
       ctx.update.channel_post != undefined
         ? ctx.update.channel_post.chat.id
-        : "no soy un grupo";
-    console.log(channelId);
-
-    // Cada hora tiempo se ejecuta este metodo
-    cronTaskCryptos.start();
-
-    // A las 10, 12 y 15 horas se ejecuta
-    cronTaskDolar.start();
-  }
-});
-
-const cronTaskCryptos = cron.schedule(
-  "1 * * * *",
-  () => {
-    console.log("Ejecutando cronTaskCryptos");
-    binance.prevDay(false, (error, prevDay) => {
-      // Ordena segun volumen y filtro los 15 mayores
-      prevDay.sort(
-        (a, b) => parseFloat(b.quoteVolume) - parseFloat(a.quoteVolume)
-      );
-      prevDay = prevDay.filter((pair) => pair.symbol.indexOf("USDT") > 1);
-
-      setCryptosMessage(prevDay);
-      console.log(cryptoRes);
-      // Envia el mensaje al canal
-      bot.telegram.sendMessage(channelId, cryptoRes);
-      cryptoRes = "";
-    });
-  },
-  {
-    scheduled: false,
-  }
-);
-
-function setCryptosMessage(prevDay) {
-  for (let obj of prevDay.slice(0, 15)) {
-    let symbol = obj.symbol.slice(0, obj.symbol.indexOf("USDT"));
-    //let symbol = obj.symbol;
-    let volume =
-      parseInt(obj.quoteVolume) > 1000000
-        ? parseInt(parseInt(obj.quoteVolume) / 1000000) + "M"
-        : obj.quoteVolume;
-    cryptoRes += `${symbol}  vol: ${volume}  change: ${obj.priceChangePercent}%\n`;
+        : "no soy un channel";
+    console.log(`Channel ID =${channelId}`);
   }
 }
 
+// A las 9:50, 11:50 y 14:50 horas se ejecuta
 const cronTaskDolar = cron.schedule(
-  "5 10,12,15 * * *",
+  "50 9,11,14 * * *",
   () => {
+    getDollars();
+  },
+  {
+    timezone: "America/Argentina/Buenos_Aires",
+  }
+);
+
+// Cada hora tiempo se ejecuta este metodo
+const cronTaskCryptos = cron.schedule("5 * * * *", () => {
+  console.log("Ejecutando cronTaskCryptos");
+  getCryptos();
+});
+
+// Obtiene el valor de los dolares sincronicamente
+bot.hears("/usd", (ctx) => {
+  setChannelId(ctx);
+  getDollars();
+});
+
+// Obtiene el valor de las crypto sincronicamente
+bot.hears("/status", (ctx) => {
+  setChannelId(ctx);
+  getCryptos();
+});
+
+function getDollars() {
+  if (channelId) {
     axios
       .get(url)
       .then((urlResponse) => {
@@ -96,11 +87,37 @@ BLUE compra: ${blue[0]}$ venta: ${blue[1]}$ promedio: ${blue[2]}$`
       .catch(() => {
         ctx.reply(`Hubo un error al intentar obtener los valores`);
       });
-  },
-  {
-    scheduled: false,
-    timezone: "America/Argentina/Buenos_Aires",
   }
-);
+}
+
+function getCryptos() {
+  if (channelId) {
+    binance.prevDay(false, (error, prevDay) => {
+      // Ordena segun volumen y filtro los 15 mayores
+      prevDay.sort(
+        (a, b) => parseFloat(b.quoteVolume) - parseFloat(a.quoteVolume)
+      );
+      prevDay = prevDay.filter((pair) => pair.symbol.indexOf("USDT") > 1);
+
+      setCryptosMessage(prevDay);
+      console.log(cryptoRes);
+      // Envia el mensaje al canal
+      bot.telegram.sendMessage(channelId, cryptoRes);
+      cryptoRes = "";
+    });
+  }
+}
+
+function setCryptosMessage(prevDay) {
+  for (let obj of prevDay.slice(0, 15)) {
+    let symbol = obj.symbol.slice(0, obj.symbol.indexOf("USDT"));
+    //let symbol = obj.symbol;
+    let volume =
+      parseInt(obj.quoteVolume) > 1000000
+        ? parseInt(parseInt(obj.quoteVolume) / 1000000) + "M"
+        : obj.quoteVolume;
+    cryptoRes += `${symbol}  vol: ${volume}  change: ${obj.priceChangePercent}%\n`;
+  }
+}
 
 bot.launch();
