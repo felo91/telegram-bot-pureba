4aafb6b175d590481df7740a5d27f992bd714d57
felo91 <felipe@wecover.com>
2020-08-24 12:35:55 -0300

    ChannelID se vuelve constante

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cheerio = require("cheerio");
 const cron = require("node-cron");
 const binance = require("node-binance-api")();
 
+const TOKEN = "1093840907:AAHd8jvFLeR0kgOVmHXoCDbw2xqgAYRGMWU";
+const channelId = -1001442441694;
+
 const bot = new Telegraf(TOKEN, {
   username: "FinanzasHoy",
   channelMode: true,
@@ -13,11 +16,10 @@ const url = "https://www.dolar.blue/";
 const urlBtc = "https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD";
 let oficial = [];
 let blue = [];
-let channelId;
+
 let cryptoRes = "";
 
 bot.on("text", (ctx) => {
-  setChannelId(ctx);
   if (ctx.update.channel_post.text == "/usd") {
     // Obtiene el valor de los dolares sincronicamente
     getDollars();
@@ -26,16 +28,6 @@ bot.on("text", (ctx) => {
     getCryptos();
   }
 });
-
-function setChannelId(ctx) {
-  if (!channelId) {
-    channelId =
-      ctx.update.channel_post != undefined
-        ? ctx.update.channel_post.chat.id
-        : "no soy un channel";
-    console.log(`Channel ID =${channelId}`);
-  }
-}
 
 // A las 9:50, 11:50 y 14:50 horas se ejecuta
 const cronTaskDolar = cron.schedule(
@@ -56,51 +48,47 @@ const cronTaskCryptos = cron.schedule("5 * * * *", () => {
 });
 
 function getDollars() {
-  if (channelId) {
-    axios
-      .get(url)
-      .then((urlResponse) => {
-        const $ = cheerio.load(urlResponse.data);
-        // Se guardan los datos sacados de la web en el array "oficial"
-        $("p.price").each((i, element) => {
-          oficial.push($(element).html());
-        });
-        // Se guardan los datos sacados de la web en el array "blue"
-        $("p.price-blue").each((i, element) => {
-          blue.push($(element).html());
-        });
-      })
-      .then(() => {
-        // Una vez obtenidos los datos se envia el mensaje
-        bot.telegram.sendMessage(
-          channelId,
-          `OFICIAL compra: ${oficial[0]}$ venta: ${oficial[1]}$ promedio: ${oficial[2]}$ 
-BLUE compra: ${blue[0]}$ venta: ${blue[1]}$ promedio: ${blue[2]}$`
-        );
-        oficial = [];
-        blue = [];
-      })
-      .catch(() => {
-        ctx.reply(`Hubo un error al intentar obtener los valores`);
+  axios
+    .get(url)
+    .then((urlResponse) => {
+      const $ = cheerio.load(urlResponse.data);
+      // Se guardan los datos sacados de la web en el array "oficial"
+      $("p.price").each((i, element) => {
+        oficial.push($(element).html());
       });
-  }
+      // Se guardan los datos sacados de la web en el array "blue"
+      $("p.price-blue").each((i, element) => {
+        blue.push($(element).html());
+      });
+    })
+    .then(() => {
+      // Una vez obtenidos los datos se envia el mensaje
+      bot.telegram.sendMessage(
+        channelId,
+        `OFICIAL compra: ${oficial[0]}$ venta: ${oficial[1]}$ promedio: ${oficial[2]}$ 
+BLUE compra: ${blue[0]}$ venta: ${blue[1]}$ promedio: ${blue[2]}$`
+      );
+      oficial = [];
+      blue = [];
+    })
+    .catch(() => {
+      ctx.reply(`Hubo un error al intentar obtener los valores`);
+    });
 }
 
 function getCryptos() {
-  if (channelId) {
-    binance.prevDay(false, (error, prevDay) => {
-      // Ordena segun volumen y filtro los 15 mayores
-      prevDay.sort(
-        (a, b) => parseFloat(b.quoteVolume) - parseFloat(a.quoteVolume)
-      );
-      prevDay = prevDay.filter((pair) => pair.symbol.indexOf("USDT") > 1);
-      setCryptosMessage(prevDay);
-      console.log(cryptoRes);
-      // Envia el mensaje al canal
-      bot.telegram.sendMessage(channelId, cryptoRes);
-      cryptoRes = "";
-    });
-  }
+  binance.prevDay(false, (error, prevDay) => {
+    // Ordena segun volumen y filtro los 15 mayores
+    prevDay.sort(
+      (a, b) => parseFloat(b.quoteVolume) - parseFloat(a.quoteVolume)
+    );
+    prevDay = prevDay.filter((pair) => pair.symbol.indexOf("USDT") > 1);
+    setCryptosMessage(prevDay);
+    console.log(cryptoRes);
+    // Envia el mensaje al canal
+    bot.telegram.sendMessage(channelId, cryptoRes);
+    cryptoRes = "";
+  });
 }
 
 function setCryptosMessage(prevDay) {
