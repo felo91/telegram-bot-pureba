6e8ed103ec069d1361142f08712eadfe427cb1a7
maxifwortman <maxifwortman@github.com>
2020-09-18 19:51:47 -0300

    bot working

--- index.js
+++ index.js
@@ -1,107 +1,157 @@
-const { Telegraf } = require("telegraf");
-const axios = require("axios");
-const cheerio = require("cheerio");
+const Binance = require('node-binance-api');
+var map = require('collection-map');
 const cron = require("node-cron");
-const binance = require("node-binance-api")();
+const { Telegraf } = require("telegraf");
+const moment = require('moment')
+const fs = require('fs');
+let jsonData = require('./persistence.json');
 
-const TOKEN = "1093840907:AAHd8jvFLeR0kgOVmHXoCDbw2xqgAYRGMWU";
-const channelId = -1001442441694;
+test = true
 
-const bot = new Telegraf(TOKEN, {
-  username: "FinanzasHoy",
-  channelMode: true,
+upchart = `\u{1F4C8}`
+downchart = `\u{1F4C9}`
+
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+    username: "FinanzasHoy",
+    channelMode: true,
 });
 
-const url = "https://www.dolar.blue/";
-const urlBtc = "https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD";
-let oficial = [];
-let blue = [];
-
-let cryptoRes = "";
-
-bot.on("text", (ctx) => {
-  if (ctx.update.channel_post.text == "/usd") {
-    // Obtiene el valor de los dolares sincronicamente
-    getDollars();
-  } else if (ctx.update.channel_post.text == "/status") {
-    // Obtiene el valor de las crypto sincronicamente
-    getCryptos();
-  }
+const binance = new Binance().options({
+    APIKEY: process.env.APIKEY,
+    APISECRET: process.env.APISECRET
 });
 
-// A las 9:50, 11:50 y 14:50 horas se ejecuta
-const cronTaskDolar = cron.schedule(
-  "50 9,11,14 * * *",
-  () => {
-    getDollars();
-  },
-  {
-    scheduled: true,
-    timezone: "America/Argentina/Buenos_Aires",
-  }
-);
 
-// Cada hora tiempo se ejecuta este metodo
-const cronTaskCryptos = cron.schedule("5 * * * *", () => {
-  console.log("Ejecutando cronTaskCryptos");
-  getCryptos();
+//Prod
+let channelId = process.env.CHANNEL_ID
+//Test
+let testChannelId = process.env.TEST_CHANNEL_ID
+
+
+function sendMessageToChannel(message) {
+    if (test) {
+        bot.telegram.sendMessage(testChannelId, message, { parse_mode: 'HTML' });
+    } else {
+        bot.telegram.sendMessage(channelId, message, { parse_mode: 'HTML' });
+    }
+    bot.telegram.sendMessage(channelId, message, { parse_mode: 'HTML' });
+
+    console.log(message)
+}
+
+var res = new Map();
+var percentage = new Map();
+var coinsMap = new Map();
+var finalMap = new Map();
+var updateMap = new Map()
+var notificationMap = new Map()
+
+var coinPer = new Map();
+
+var coinInfo = {}
+
+
+global.ticker = {};
+
+// Get 24h price change statistics for all symbols
+binance.websockets.prevDay(false, function(error, obj) {
+    global.ticker[obj.symbol] = obj;
+    //console.log(obj)
+    if (coinsMap.has(obj.symbol)) {
+        var coin = {}
+        coin.volume = coinsMap.get(obj.symbol).quoteVolume
+        coin.percentage = obj.percentChange
+        coin.price = obj.bestBid
+        coin.symbol = obj.symbol
+        finalMap.set(obj.symbol, coin)
+    }
 });
 
-function getDollars() {
-  axios
-    .get(url)
-    .then((urlResponse) => {
-      const $ = cheerio.load(urlResponse.data);
-      // Se guardan los datos sacados de la web en el array "oficial"
-      $("p.price").each((i, element) => {
-        oficial.push($(element).html());
-      });
-      // Se guardan los datos sacados de la web en el array "blue"
-      $("p.price-blue").each((i, element) => {
-        blue.push($(element).html());
-      });
-    })
-    .then(() => {
-      // Una vez obtenidos los datos se envia el mensaje
-      bot.telegram.sendMessage(
-        channelId,
-        `OFICIAL compra: ${oficial[0]}$ venta: ${oficial[1]}$ promedio: ${oficial[2]}$ 
-BLUE compra: ${blue[0]}$ venta: ${blue[1]}$ promedio: ${blue[2]}$`
-      );
-      oficial = [];
-      blue = [];
-    })
-    .catch(() => {
-      ctx.reply(`Hubo un error al intentar obtener los valores`);
-    });
+binance.websockets.miniTicker(markets => {
+    //console.log(markets)
+    markets = Object.keys(markets).filter(key => key.indexOf("USDT") > 1).reduce((obj, key) => {
+        obj[key] = markets[key];
+        return obj;
+    }, {});
+    for (var i in markets) {
+        res.set(i, markets[i].quoteVolume)
+        coinsMap.set(i, markets[i])
+    }
+});
+
+function trimName(coin) {
+    var value = {}
+    if (symbol.indexOf("USDT") > 1) {
+        value.symbol = symbol.slice(0, coin.symbol.indexOf("USDT"));
+        value.pair = "USDT"
+    } else if (symbol.indexOf("BTC") > 1) {
+        value.symbol = symbol.slice(0, coin.symbol.indexOf("BTC"));
+        value.pair = "BTC"
+    }
+    if (symbol.indexOf("ETH") > 1) {
+        value.symbol = symbol.slice(0, coin.symbol.indexOf("ETH"));
+        value.pair = "ETH"
+    }
 }
 
-function getCryptos() {
-  binance.prevDay(false, (error, prevDay) => {
-    // Ordena segun volumen y filtro los 15 mayores
-    prevDay.sort(
-      (a, b) => parseFloat(b.quoteVolume) - parseFloat(a.quoteVolume)
-    );
-    prevDay = prevDay.filter((pair) => pair.symbol.indexOf("USDT") > 1);
-    setCryptosMessage(prevDay);
-    console.log(cryptoRes);
-    // Envia el mensaje al canal
-    bot.telegram.sendMessage(channelId, cryptoRes);
-    cryptoRes = "";
-  });
+function orderByPercentage(coinMap) {
+    var mapAsc = new Map([...coinMap].sort(
+        (a, b) => parseFloat(coinMap.get(b[0]).percentage) - parseFloat(coinMap.get(a[0]).percentage)
+    ));
+    return mapAsc
 }
 
-function setCryptosMessage(prevDay) {
-  for (let obj of prevDay.slice(0, 15)) {
-    let symbol = obj.symbol.slice(0, obj.symbol.indexOf("USDT"));
-    //let symbol = obj.symbol;
-    let volume =
-      parseInt(obj.quoteVolume) > 1000000
-        ? parseInt(parseInt(obj.quoteVolume) / 1000000) + "M"
-        : obj.quoteVolume;
-    let price = Math.round(obj.lastPrice * 10) / 10;
-    cryptoRes += `${symbol} $${price} vol: ${volume}  change: ${obj.priceChangePercent}%\n`;
-  }
-}
+cron.schedule("*/3 * * * * *", function() {
+    var mapAsc = orderByPercentage(finalMap);
+    //console.log(mapAsc)
+    var b = [...mapAsc].slice(0, 15)
+    var send = false
+    var message = "Coin Trading Update\n"
+    for (i in b) {
+        coin = b[i][1]
+        icon = upchart
+        if (Math.abs(parseFloat(coin.percentage)) > 5) {
+            if (jsonData[coin.symbol] != undefined) {
+                if (coin.price / jsonData[coin.symbol].price > 0.95 && coin.price / jsonData[coin.symbol].price < 1.05) {
+                    continue
+                }
+                if (coin.price < jsonData[coin.symbol].price) {
+                    icon = downchart
+                }
+            }
+            var now = moment();
+            var updated = false
+            if (jsonData[coin.symbol] != undefined) {
+                notification = jsonData[coin.symbol]
+                t = moment(notification.updated, "DD MM YYYY HH:mm:ss")
+                if (now.diff(t, 'seconds') < 3) {
+                    updated = true
+                }
+            }
+            if (!updated) {
+                let symbol = coin.symbol.slice(0, coin.symbol.indexOf("USDT"));
+                let volume = parseInt(coin.volume) > 1000000 ? parseInt(parseInt(coin.volume) / 1000000) + "M" : coin.volume;
+                volume = Math.round(coin.volume);
+                let price = Math.round(coin.price * 100) / 100;
+                let percentage = Math.round(coin.percentage * 100) / 100;
+                message += (`${icon} ${symbol} $${price} 24HVol: ${volume} ${percentage}%\n`)
+                send = true
+                notification = {}
+                notification.updated = now.format('DD MM YYYY HH:mm:ss')
+                notification.percentage = coin.percentage
+                notification.price = coin.price
+                jsonData[coin.symbol] = notification
+                let data = JSON.stringify(jsonData, null, 2);
+                fs.writeFile('persistence.json', data, (err) => {
+                    if (err) throw err;
+                    console.log('Data written to file');
+                });
+            }
+        }
+    }
+    if (send) {
+        sendMessageToChannel(message)
+    }
 
-bot.launch();
+    //console.log("---ticker---");
+});
